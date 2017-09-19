ff6e078f672199fe6fd5f8b5d3b91c8152254c62
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-09-19 14:49:43 +0200

    add routes and views

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 
+const routes = require('./routes');
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('it works');
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}`);
