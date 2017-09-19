50fe20ea317c6fcc87084d80f316d88b4b72e6c9
Ania Brajtkopf <aniabrajtkopf@gmail.com>
2017-09-19 14:33:08 +0200

    start server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('it works');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}`);
+});
