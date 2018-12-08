21d27ae40ab7b16b8515d2ab984591100ce9f825
Ania <aniabrajtkopf@gmail.com>
2018-12-08 17:31:59 +0100

    added DELETE quote by id

--- controllers/quoteController.js
+++ controllers/quoteController.js
@@ -1,30 +1,32 @@
-const mongoose = require('mongoose');
-const Quote = mongoose.model('Quote');
+const mongoose = require("mongoose");
+const Quote = mongoose.model("Quote");
 
 exports.getQuotes = async (req, res) => {
   const quotes = await Quote.find();
   res.json(quotes);
-}
+};
 
 exports.createQuote = async (req, res) => {
-  const lastQuote = await Quote.find().sort({ index: -1 }).limit(1);
-  const index = lastQuote[0]['index'] + 1;
+  const lastQuote = await Quote.find()
+    .sort({ index: -1 })
+    .limit(1);
+  const index = lastQuote[0]["index"] + 1;
   const { text, author } = req.body;
-  const quote = await (new Quote({index, text, author})).save();
+  const quote = await new Quote({ index, text, author }).save();
   res.json(quote);
-}
+};
 
 exports.getRandomQuote = async (req, res) => {
   const quote = await Quote.getRandomQuote();
   res.json(quote);
-}
+};
 
 exports.getQuoteById = async (req, res) => {
   const quote = await Quote.find({ index: req.params.id });
   res.json(quote);
-}
+};
 
-// exports.deleteQuoteById = async (req, res) => {
-//   await Quote.deleteOne({ index: req.params.id });
-//   res.json({ message: 'Successfully deleted.'});
-// }
+exports.deleteQuoteById = async (req, res) => {
+  await Quote.deleteOne({ index: req.params.id });
+  res.json({ message: "Successfully deleted." });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const Quote = mongoose.model('Quote');
+const Quote = mongoose.model("Quote");
 const router = express.Router();
 
-const quoteController = require('../controllers/quoteController');
-const { catchErrors } = require('../helpers');
+const quoteController = require("../controllers/quoteController");
+const { catchErrors } = require("../helpers");
 
-const { example1, example2, example3, example4 } = require('../helpers');
+const { example1, example2, example3, example4 } = require("../helpers");
 
-router.get('/', (req, res) => {
-  res.render('index', { example1, example2, example3, example4 });
+router.get("/", (req, res) => {
+  res.render("index", { example1, example2, example3, example4 });
 });
 
-router.post('/api/quotes', catchErrors(quoteController.createQuote));
+router.post("/api/quotes", catchErrors(quoteController.createQuote));
 
-router.get('/api/quotes', catchErrors(quoteController.getQuotes));
+router.get("/api/quotes", catchErrors(quoteController.getQuotes));
 
-router.get('/api/quotes/random', catchErrors(quoteController.getRandomQuote));
+router.get("/api/quotes/random", catchErrors(quoteController.getRandomQuote));
 
-router.get('/api/quotes/:id', catchErrors(quoteController.getQuoteById));
+router.get("/api/quotes/:id", catchErrors(quoteController.getQuoteById));
 
+router.delete("/api/quotes/:id", catchErrors(quoteController.deleteQuoteById));
 
 module.exports = router;
